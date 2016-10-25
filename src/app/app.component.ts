6fa92899d94f6b6a264bd585a401518a0074e2b6
Louis Huang <0563louishuang@gmail.com>
2016-10-25 12:24:24 -0700

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+
+import { RecipesComponent } from './recipes';
+
+import { ShoppingListComponent } from './shopping-list';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ShoppingListAddComponent } from "./shopping-list-add.component";
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html'
+
+})
+export class ShoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
